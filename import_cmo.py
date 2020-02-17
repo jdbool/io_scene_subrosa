b298e68f2fad38fd0355e372efc37567206165bf
jdb <44955015+jdbool@users.noreply.github.com>
2020-02-16 20:51:06 -0800

    UV importing

--- import_cmo.py
+++ import_cmo.py
@@ -1,6 +1,7 @@
 import bpy
 import bmesh
 from struct import unpack
+from math import pi
 
 
 def load(context, filepath):
@@ -12,12 +13,13 @@ def load(context, filepath):
 
         vertices = []
         faces = []
+        vertex_uvs = []
 
         (vertex_count,) = unpack('<i', f.read(4))
         for _ in range(vertex_count):
-            pos = unpack('<fff', f.read(4 * 3))
-            vertices.append(pos)
-            f.read(4 * 3)
+            vertices.append(unpack('<fff', f.read(4 * 3)))
+            vertex_uvs.append(unpack('<ff', f.read(4 * 2)))
+            f.read(4)
 
         (face_count,) = unpack('<i', f.read(4))
         for _ in range(face_count):
@@ -41,6 +43,9 @@ def load(context, filepath):
             faces
         )
 
+        mesh.uv_layers.new(do_init=False)
+        mesh.uv_layers[-1].data.foreach_set('uv', [uv for pair in [vertex_uvs[l.vertex_index] for l in mesh.loops] for uv in pair])
+
         bm = bmesh.new()
         bm.from_mesh(mesh)
 
@@ -53,6 +58,7 @@ def load(context, filepath):
         mesh.update()
 
         obj = bpy.data.objects.new(name, mesh)
+        obj.rotation_euler = (pi / 2, 0.0, 0.0)
         
         view_layer = context.view_layer
         collection = view_layer.active_layer_collection.collection
