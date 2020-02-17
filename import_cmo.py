63389f37329a389533f02ddce3d2423e36783f52
jdb <44955015+jdbool@users.noreply.github.com>
2020-02-16 21:41:42 -0800

    Cleaned up

--- import_cmo.py
+++ import_cmo.py
@@ -1,7 +1,5 @@
-import bpy
-import bmesh
 from struct import unpack
-from math import pi
+from . import shared
 
 
 def load(context, filepath):
@@ -37,38 +35,6 @@ def load(context, filepath):
             faces.append(tuple(vertex_indices))
             f.read(8 if version > 1 else 4)
         
-        name = bpy.path.display_name_from_filepath(filepath)
-        
-        mesh = bpy.data.meshes.new(name)
-        mesh.from_pydata(
-            vertices,
-            (),
-            faces
-        )
-
-        mesh.uv_layers.new(do_init=False)
-        mesh.uv_layers[-1].data.foreach_set('uv', [uv for pair in [vertex_uvs[l.vertex_index] for l in mesh.loops] for uv in pair])
-
-        bm = bmesh.new()
-        bm.from_mesh(mesh)
-
-        bmesh.ops.remove_doubles(bm, verts=bm.verts[:], dist=0.001)
-
-        bm.to_mesh(mesh)
-        bm.free()
-
-        mesh.validate()
-        mesh.update()
-
-        obj = bpy.data.objects.new(name, mesh)
-        obj.rotation_euler = (pi / 2, 0.0, 0.0)
-        
-        view_layer = context.view_layer
-        collection = view_layer.active_layer_collection.collection
-
-        collection.objects.link(obj)
-        obj.select_set(True)
-
-        view_layer.update()
+        shared.load_mesh(context, filepath, vertices, faces, vertex_uvs)
     
     return {'FINISHED'}
